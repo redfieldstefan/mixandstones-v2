0d330f0ecaa2d787b1fb272c31b942575617c7bf
Stefan Redfield <redfield.stefan@gmail.com>
2015-09-05 19:48:50 -0700

    create cocktail routes

---
+++ routes/api.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var Cocktail = require('../models/cocktail_model');
+var Ingredient = require('../models/ingredient_model');
+
+var COCKTAIL_URL = '/api/cocktails';
+var INGREDIENT_URL = '/api/ingredients';
+
+var _urlify = function(cocktailName) {
+  return cocktailName
+    .toLowerCase()
+    .replace(/\s/g, '-')
+    .replace(/[^A-Za-Z0-9\-]/g, '')
+    .replace(/-{2,}/g, '-');
+};
+
+var _prepForDb = function(cocktail) {
+  return {
+    name: cocktail.name,
+    url: urlify(cocktail.name)
+    description: cocktail.description ?
+      cocktail.description :
+      '',
+    ingredients: cocktail.ingredients ?
+      cocktail.ingredients.sort() :
+      []
+  };
+};
+
+module.exports = function(app) {
+
+  app.post(COCKTAIL_URL, function(req, res) {
+    var cocktail = new Cocktail(_prepForDb(req.body));
+    cocktail.save(function(err, dbRes) {
+      if(err) {
+        console.log(err);
+        return dbRes.status(500).json({msg: 'server error'});
+      }
+      return res.statu(200).json(dbRes);
+    });
+  });
+
+  app.get(COCKTAIL_URL, function(req, res) {
+    Cocktail.find({}, function(err, dbRes) {
+      if(err) {
+        console.log(err);
+        return dbRes.status(500).json({msg: 'server error'});
+      }
+      return res.statu(200).json(dbRes);
+    });
+  });
+
+  app.get(COCKTAIL_URL + ':id', function(req, res) {
+    Cocktail.find({_id: req.params.id}, function(err, dbRes) {
+      if(err) {
+        console.log(err);
+        return dbRes.status(500).json({msg: 'server error'});
+      }
+      return res.statu(200).json(dbRes);
+    });
+  });
+
+  app.put(COCKTAIL_URL + ':id', function(req, res) {
+    var updatedCocktail = _prepForDb(req.body);
+    Cocktail.update({_id: req.params.id}, updatedCocktail, function(err, dbRes) {
+      if(err) {
+        console.log(err);
+        return dbRes.status(500).json({msg: 'server error'});
+      }
+      return res.statu(200).json(dbRes);
+    });
+  });
+
+  app.delete(COCKTAIL_URL + ':id', function(req, res) {
+    Cocktail.remove({_id: req.params.id}, function(err) {
+      if(err) {
+        console.log(err);
+        return dbRes.status(500).json({msg: 'server error'});
+      }
+      return res.statu(200).json('Cocktail Deleted');
+    });
+  });
+
+};
