369061f2d77b89aa1a4ba6686330bccfd0102714
David Davidson <daviddavidson.1988@gmail.com>
2015-09-10 14:32:38 -0700

    Add a little state to cocktail.jsx, to show that it's not interactive yet

--- client/components/cocktail.jsx
+++ client/components/cocktail.jsx
@@ -2,8 +2,35 @@ const React = require('react');
 
 class Cocktail extends React.Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      numClicks: 0
+    };
+  }
+
+  _handleClick () {
+    const currentNumClicks = this.state.numClicks;
+    this.setState('numClicks', currentNumClicks + 1);
+  }
+
+  _getNumClicks () {
+    const numClicks = this.state.numClicks;
+    return numClicks === 1 ?
+      numClicks + ' time' :
+      numClicks + ' times';
+  }
+
   render () {
-    return <h1>{this.props.name}</h1>;
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <button onClick={this._handleClick.bind(this)}>
+          I've been clicked {this._getNumClicks()} (and I don't work)
+        </button>
+      </div>
+    );
   }
 
 }
